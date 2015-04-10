2fea655ea5b374c518030347d861593da3d721d3
Amr Ragab <amrstein@hotmail.com>
2015-04-10 15:36:25 +0200

    First Commit

---
+++ apps/Sticker/android/native/assets/www/default/worklight/checksum.js
@@ -0,0 +1,2 @@
+var WL_CHECKSUM = {"checksum":887840632,"date":1428672933424,"machine":"Amro"};
+/* Date: Fri Apr 10 15:35:33 EET 2015 */
---
+++ apps/Sticker/android/native/gen/com/Sticker/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.Sticker;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
