ca4692e14a8f6fe09e444efef5add75be3c8ef6d
FawzY93 <m_fawzy93@hotmail.com>
2015-05-16 04:09:08 +0300

    addAlbum

--- apps/Sticker/common/js/album.js
+++ apps/Sticker/common/js/album.js
@@ -1,32 +1,154 @@
-function intialize() {
-	
-}
-
-
-function loadSticks() {
-	//load all the sticks for the user
-}
-
-function displaySticks(sticks) {
-	//display sticks in their positions
-}
-
-function addAlbum() {
-	//add an album for the user
-}
-
+ 
 $(function(){
+
+var userId;
+var href="#";
+
+$("#albumSubmit").click(function(){
+		if(href!=="#")
+			$("#albumSubmit").attr("href",href);	
+		else
+		{
+			$("#albumForm").submit();
+		}
+	});
+
+
+function intialize() {
+	userId= parseInt(getActiveUser());
+	//userId=2;
+ 	getUserSticks(userId);
+}
+
+
+/////////////////////////////////////////////////////////// getUserSticks ////////////////////////////////////////////////////////
+function getUserSticks(userId){
+		var invocationData = {
+			adapter : 'StickerStore',
+			procedure : 'getUserSticks',
+			parameters : [userId]
+		};
+
+		WL.Client.invokeProcedure(invocationData,{
+			onSuccess : getUserSticksSuccess,
+			onFailure : getUserSticksFailure
+		});
+	}
+
+
+function getUserSticksSuccess(result){
+		var resultLength=result.invocationResult.resultSet.length;	
+		if(resultLength==0)
+		{
+		alert("No sticks found, album can't be created");
+		href="home.html";
+		$("#albumSubmit").click();	
+		}
+		else 
+			displaySticks(result.invocationResult.resultSet);		
+	}
+
+function getUserSticksFailure(result){
+		alert("Error loading sticks");
+		href="home.html";
+		$("#albumSubmit").click();
 	
+	}	
+
+//////////////////////////////////////////////////////// displaySticks /////////////////////////////////////////////////////////////
+function displaySticks(sticks) {
+
+	var sticksDiv=$("#existingStickList");
+	for (var i = 0; i < sticks.length; i++) {
+		var Id=String(sticks[i].stickId);
+		
+		var html1=$("<div class='right-checkbox'>").append("<input type='checkbox' name='checkbox-h-2a' class='albumCheck' id=checkbox-"+Id+">");
+		var html2=$("<div class=stickTitle>"+sticks[i].stickTitle+"</div>");
+		var html3=$("<div class=ui-grid-a>")
+		.append("<div class='ui-black-a stickTime' title='stickTime'>"+sticks[i].stickTime,"<div class='ui-block-b stickLoc' title='stickLoc'>"+sticks[i].stickLocation);
+		
+		var html=$("<div id=stick"+Id+" class='stick ui-body ui-corner-all'>").append(html1,html2,html3);
+		sticksDiv.append(html);
+	}
+
+}
+//////////////////////////////////////////////////// addAlbum /////////////////////////////////////////////////////////////////////////
+function addAlbum(userId,albumTitle, albumInfo) {
+		var invocationData = {
+			adapter : 'StickerStore',
+			procedure : 'addAlbum',
+			parameters : [userId,albumTitle, albumInfo]
+		};
+
+		WL.Client.invokeProcedure(invocationData,{
+			onSuccess : addAlbumSuccess,
+			onFailure : addAlbumFailure
+		});
+	}
+function addAlbumSuccess(result){
+		//var albumId=result.invocationResult.resultSet.albumId;
+		var albumId=1;
+		addSticksToAlbum(albumId)
+	}
+
+function addAlbumFailure(result){
+		alert("Failed to add album");
+	}	
+	
+////////////////////////////////////////////////////////////// addStickstoAlbum ///////////////////////////////////////////////////////
+function addSticksToAlbum(albumId){
+	var checkboxes=$(".albumCheck");
+	for(var i = 0; i < checkboxes.length; i++){
+	
+		if(checkboxes[i].checked){
+ 			var stickId=parseInt(checkboxes[i].id.split('-')[1]);
+			updateUSAStatement(userId,albumId, stickId);
+		}
+	}
+	href="home.html";
+	$("#albumSubmit").click();		
+}
+function updateUSAStatement(userId,albumId, stickId){
+				var invocationData = {
+					adapter : 'StickerStore',
+					procedure : 'updateUSA',
+					parameters : [userId, albumId, stickId]
+				};
+
+				WL.Client.invokeProcedure(invocationData,{
+					onSuccess : updateUSASuccess,
+					onFailure : updateUSAFailure
+				});
+			}
+function updateUSASuccess(result){	
+	}
+
+function updateUSAFailure(result){
+
+	}		
+
+///////////////////////////////////////////////////////////////// page functions //////////////////////////////////////////////////////	
 	intialize();
 	
-	$("#albumSubmit").click(function(){
-
-		$("#albumForm").submit();
-	});
 	
 	$("#albumForm").submit(function(){
-		alert("Album!!");
+		albumTitle=$("#albumName").val();
+		albumInfo=$("#albumNotes").val();
+		if(albumTitle==""){
+			alert("Ablum's name cant be empty u should fill it");
+			return false;
+		}
+		else if(albumInfo==""){
+			alert("Ablum's notes cant be empty u should fill it");
+			return false;
+		}
+		else if($(".albumCheck:checked").length==0)
+		{
+			alert("please check at least one stick");
+			return false;
 
+		}
+		else addAlbum(userId,albumTitle,albumInfo);
 	});
 
 	});
--- apps/Sticker/common/js/editAlbum.js
+++ apps/Sticker/common/js/editAlbum.js
@@ -1,17 +1,81 @@
 function intialize() {
-	
+	// userId= getActiveUser();
 }
 
 
 function loadAlbum() {
+		function getUserAlbumSticksStatement(userId,albumId){
+		var invocationData = {
+			adapter : 'StickerStore',
+			procedure : 'getUserAlbumSticksStatement',
+			parameters : [userId,albumId]
+		};
+
+		WL.Client.invokeProcedure(invocationData,{
+			onSuccess :getUserAlbumSuccess,
+			onFailure : getUserAlbumFailure
+		});
+	}
+	function getUserAlbumSuccess(result){
+		alert("getUserAlbum: Success!")	
+		displaySticks(result.invocationResult.resultSet)
+	//	writeCache(userId);
+		
+	}
+
+	function getUserAlbumFailure(result){
+		alert("getUserAlbum: Failure!")
+		
+	
+	}	
 	//load the album information
 }
 
+
 function loadSticks() {
+		function getUserSticks(userId){
+		var invocationData = {
+			adapter : 'StickerStore',
+			procedure : 'getUserSticks',
+			parameters : [userId]
+		};
+
+		WL.Client.invokeProcedure(invocationData,{
+			onSuccess : getUserSticksSuccess,
+			onFailure : getUserSticksFailure
+		});
+	}
+	function getUserSticksSuccess(result){
+		alert("loadSticks: Success!")	
+		displaySticks(result.invocationResult.resultSet)
+	//	writeCache(userId);
+		
+	}
+
+	function getUserSticksFailure(result){
+		alert("loadSticks: Failure!")
+		
+	
+	}	
 	//load all the sticks for the user
 }
 
+
+
 function displaySticks(sticks) {
+	var sticksDiv=$("#existingStickList");
+	for (var i = 0; i < sticks.length; i++) {
+		sticksDiv.append("<div class='stick ui-body ui-corner-all'>
+				<div class='right-checkbox'>
+					<input type='checkbox' name='checkbox-h-2a' id='checkbox-h-2a'>
+				</div>
+				<div class='stickTitle'>"+sticks[i].stickTitle+"</div>
+				<div class='ui-grid-a'>
+					<div class='ui-black-a stickTime' title='stickTime'>"+sticks[i].stickTime+"</div>
+					<div class='ui-block-b stickLoc' title='stickLoc'>"+sticks[i].stickLocation+"</div>
+				</div>
+			</div>");
+	}
 	//display sticks in their positions
 }
 
