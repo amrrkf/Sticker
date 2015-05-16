cc617b3f3abc429c58df6246f1a814adc76282e2
Yasser-Farouk <ghost-egy1@hotmail.com>
2015-05-16 15:38:08 +0200

    get parameter func

--- apps/Sticker/common/js/main.js
+++ apps/Sticker/common/js/main.js
@@ -16,6 +16,13 @@ function wlCommonInit(){
 	// Common initialization code goes here
 	
 }
+//get parameters from link address
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
 
 
 // regular expression for mail form validation
