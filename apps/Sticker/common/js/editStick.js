509ef36a492e006373e25fa0a4aeb8cbd8504f29
Yasser-Farouk <ghost-egy1@hotmail.com>
2015-05-16 23:57:52 +0200

    editStick done

--- apps/Sticker/common/js/editStick.js
+++ apps/Sticker/common/js/editStick.js
@@ -1,40 +1,138 @@
 
+$(function(){
 
-var stickId;
+var stickId=null;
 var userId;
 var href="#";
+/////////////////////////////////////////////////Get Stick from DB////////////////////////////////////////
+function getStick(userId, stickId){
+			
+		var invocationData = {
+			adapter : 'StickerStore',
+			procedure : 'getStick',
+			parameters : [userId, stickId]
+		};
+
+		WL.Client.invokeProcedure(invocationData,{
+			onSuccess : getStickSuccess,
+			onFailure : getStickFailure
+		});
+	}
+
+	function getStickSuccess(result){
+		var stickTitle=result.invocationResult.resultSet[0].stickTitle;
+		var stickImage=result.invocationResult.resultSet[0].stickImage;
+		var stickTime=result.invocationResult.resultSet[0].stickTime;
+		var stickLocation=result.invocationResult.resultSet[0].stickLocation;
+		var stickInfo=result.invocationResult.resultSet[0].stickInfo;
+		$("#editStickName").val(stickTitle);
+		if(stickImage!="" && stickImage!=null)
+		{
+			$("#editStickImage").attr("src","/Sticker/"+stickImage);
+		}
+		$("#editStickStatus").val(stickInfo);
+		$('#editStickLocation').text('Location: '+ stickLocation);
+	    $('#editStickTime').text('Time: '+ stickTime);
+		alert("success get stick");
+		//WL.Toast.show("success get stick");
+	}
+
+	function getStickFailure(){
+		alert("error get stick");
+		//WL.Toast.show("error get stick");
+	}	
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////Update Stick///////////////////////////////////////////////////
+	//add stick functions for adapter 
+	function updateStick(stickId, stickTitle, stickImage, stickInfo){
+			
+		var invocationData = {
+			adapter : 'StickerStore',
+			procedure : 'updateStick',
+			parameters : [stickId, stickTitle, stickImage, stickInfo]
+		};
+
+		WL.Client.invokeProcedure(invocationData,{
+			onSuccess : updateStickSuccess,
+			onFailure : updateStickFailure
+		});
+	}
+
+	function updateStickSuccess(){
+	    href="home.html";
+		$("#saveStick").click();
+		var tempSrc=$(".common-stick-image").attr("src");
+		
+		if(tempSrc!==''){
+			savePic(tempSrc);
+		}
+		alert("success Update stick");
+		//WL.Toast.show("success add stick");
+	}
+
+	function updateStickFailure(response){
+		href="editStick.html";
+		$("#saveStick").click();
+		alert("error Update stick");
+		//WL.Toast.show("error add stick");
+	}	
+
+	
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
 
 function intialize() {
-	$("#imgEditStickDelete").hide();
-	userId= parseInt(getActiveUser());	
-	setTimeout(function(){
-	stickId=getParameterByName('stickId');
-	alert(stickId);
-	}, 2000);
-}
-
-
-function loadStick() {
-	//add a stick to the user sticks
-}
-
-function saveStick() {
-	//save the edits in the stick
-}
-
-$(function(){
+		$("#imgEditStickDelete").hide();
+		userId= parseInt(getActiveUser());	
 	
+		setTimeout(function(){
+			stickId=getParameterByName('stickId');
+			alert(stickId);
+			getStick(userId,stickId);
+		}, 1000);
+
+	}
+
 	intialize();
 
-	
-
 	$("#saveStick").click(function(){
-
+		if(href!=="#")
+			$("#saveStick").attr("href",href);	
+		else
+		{
+		href="#";
 		$("#editStickForm").submit();
+		}
 	});
 	
 	$("#editStickForm").submit(function(){
-		alert("edit Stick!!");
+		
+		// get form values
+		var sName=$('#editStickName').val();  //stick name
+		var imageSrc=$(".common-stick-image").attr("src");
+		var imageName;
+		if(imageSrc!==''){
+		var value=imageSrc.split("/");
+    	count=value.length;
+    	imageName= value[count-1]; //image name
+		}
+		else{
+			imageName='';
+		}
+		var sStatus=$('#editStickStatus').val(); //status
+
+		if(sName==''|| sStatus=='')	// check for empty values
+			{
+				alert('please fill the form');
+				//WL.Toast.show("please fill the form");
+				return false;
+			}
+		else{
+				updateStick(stickId, sName, imageName, sStatus); //update Stick
+		}
 	});
 	$("#takeEditStickPic").click(function(){
 		$("#editStickPopup2").popup("close");
@@ -54,5 +152,4 @@ $(function(){
 		$("#imgEditStickDelete").hide();
 	});
 
-	
-	});
+});
